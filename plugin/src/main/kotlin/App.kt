1a5b156f5d99564649c72d5f713ef63a38ab1680
Skyscx <danakis20@gmail.com>
2024-07-24 10:23:30 +0300

    add discordBot

--- plugin/src/main/kotlin/App.kt
+++ plugin/src/main/kotlin/App.kt
@@ -7,6 +7,7 @@ import discord.DiscordSRVHook
 import discord.dsbot.DiscordBot
 import functions.events.PlayerConnection
 import org.bukkit.Bukkit
+import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
@@ -19,6 +20,8 @@ lateinit var app: App
 class App : JavaPlugin(), Listener {
     lateinit var database: Database
     private lateinit var discordBot: DiscordBot
+    private lateinit var config: FileConfiguration
+
 
 
     override fun onEnable() {
@@ -42,6 +45,10 @@ class App : JavaPlugin(), Listener {
             server.pluginManager.disablePlugin(this)
             return
         }
+        //Config
+        saveDefaultConfig()
+        reloadConfig()
+        config = getConfig()
 
         //Commands
         getCommand("pay")?.setExecutor(PayCommand(database))
@@ -59,10 +66,12 @@ class App : JavaPlugin(), Listener {
             DiscordSRVHook.register()
         }
 
-        //discord.dsbot.DiscordBot
+        //DiscordBot
         discordBot = DiscordBot(database)
-        discordBot.start("MTI2NTAwMjcyMTQ4ODkyODgyOQ.GtnVS0.QhQF26tObwGDt2EDLdNqQl5rMxMeumn6p0XXJI")
+        val token = config.getString("bot-token")
+        discordBot.start(token)
     }
+    //
 
     override fun onDisable() {
         discordBot.jda.shutdown()
@@ -70,6 +79,18 @@ class App : JavaPlugin(), Listener {
             DiscordSRVHook.unregister()
         }
         database.closeConnection();
+        saveConfig()
+    }
+
+    override fun saveDefaultConfig() {
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs()
+        }
+
+        val configFile = File(dataFolder, "config.yml")
+        if (!configFile.exists()) {
+            saveResource("config.yml", false)
+        }
     }
 
 
