63daaea03ce9899ea19cd5da5b32e902c42fa980
Skyscx <danakis20@gmail.com>
2024-07-24 12:04:57 +0300

    add checkers

--- plugin/src/main/kotlin/discord/dsbot/DiscordBot.kt
+++ plugin/src/main/kotlin/discord/dsbot/DiscordBot.kt
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
 
@@ -23,59 +24,11 @@ class DiscordBot (private val database: Database): ListenerAdapter() {
 
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
         if (event.name != "hello") return
-
-        //val channel = event.channel as MessageChannel
-        //val user = event.user
-        //event.reply(event.getOption("Hello, ${user.asMention}!")!!.asString).queue()
-        //channel.sendMessage("Hello, ${user.asMention}!").queue()
-        //event.reply("Test3").queue()
-
         val channel = event.channel as MessageChannel
-//        val user = event.user
-
-        // проверяем ID канала
         if (channel.idLong != PayCommandDiscord.ALLOWED_CHANNEL_ID) {
             event.reply("Эту команду можно использовать только в <#${PayCommandDiscord.ALLOWED_CHANNEL_ID}> канале.").queue()
             return
         }
-
-        //Получение аргументов
-//        val targetMember = event.getOption("user")?.asMember ?: return
-//        val amount = event.getOption("amount")?.asLong ?: return
-
-        // проверяем, что сумма перевода положительная
-//        if (amount <= 0) {
-//            event.reply("Сумма перевода должна быть положительной.").queue()
-//            return
-//        }
-//
-
-        // Операция перевода
-//        val discordIDUser = user.id
-//        val uuid = database.getUUIDforDiscordID(discordIDUser)
-//
-//        val senderBalance = database.getPlayerBalance(uuid)
-//        if (senderBalance < amount) {
-//            user.openPrivateChannel().queue { channel ->
-//                event.reply("У вас недостаточно средств.").queue()
-//            }
-//            return
-//        }
-//
-//        val newSenderBalance = senderBalance - amount
-//        val newTargetBalance = database.getPlayerBalance(targetMember.id) + amount
-//
-//        database.setPlayerBalance(uuid, newSenderBalance.toInt())
-//        database.setPlayerBalance(targetMember.id, newTargetBalance.toInt())
-//
-//        user.openPrivateChannel().queue { channel ->
-//            channel.sendMessage("Вы перевели $amount монет игроку ${targetMember.asMention}.").queue()
-//        }
-//
-//        targetMember.user.openPrivateChannel().queue { channel ->
-//            channel.sendMessage("Игрок ${user.asMention} перевел вам $amount монет.").queue()
-//        }
-
         event.reply("Команда выполнена успешно.").queue()
 
 
@@ -85,6 +38,8 @@ class DiscordBot (private val database: Database): ListenerAdapter() {
         val commands: List<SlashCommandData> = listOf(
             Commands.slash("hello", "Says hello to the user"),
             Commands.slash("pay", "My custom command")
+                .addOption(OptionType.USER, "user", "Целевой игрок", true)
+                .addOption(OptionType.INTEGER, "amount", "Сумма перевода", true)
         )
 
         val guild = jda.getGuildById("1265001474870612068")
--- plugin/src/main/kotlin/discord/dsbot/commands/PayCommandDiscord.kt
+++ plugin/src/main/kotlin/discord/dsbot/commands/PayCommandDiscord.kt
@@ -8,11 +8,10 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 class PayCommandDiscord (private val database: Database) : ListenerAdapter() {
 
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
-
         if (event.name != "pay") return
 
         val channel = event.channel as MessageChannel
-//        val user = event.user
+        val user = event.user
 
         // проверяем ID канала
         if (channel.idLong != ALLOWED_CHANNEL_ID) {
@@ -21,45 +20,61 @@ class PayCommandDiscord (private val database: Database) : ListenerAdapter() {
         }
 
         //Получение аргументов
-//        val targetMember = event.getOption("user")?.asMember ?: return
-//        val amount = event.getOption("amount")?.asLong ?: return
+        val targetMember = event.getOption("user")?.asMember ?: return
+        println("TargetMember - $targetMember")
+        val amount = event.getOption("amount")?.asLong ?: return
+        println("Amount - $amount")
 
         // проверяем, что сумма перевода положительная
-//        if (amount <= 0) {
-//            event.reply("Сумма перевода должна быть положительной.").queue()
-//            return
-//        }
-//
-
+        if (amount <= 0) {
+            event.reply("Сумма перевода должна быть положительной.").queue()
+            return
+        }
         // Операция перевода
-//        val discordIDUser = user.id
-//        val uuid = database.getUUIDforDiscordID(discordIDUser)
-//
-//        val senderBalance = database.getPlayerBalance(uuid)
-//        if (senderBalance < amount) {
-//            user.openPrivateChannel().queue { channel ->
-//                event.reply("У вас недостаточно средств.").queue()
-//            }
-//            return
-//        }
-//
-//        val newSenderBalance = senderBalance - amount
-//        val newTargetBalance = database.getPlayerBalance(targetMember.id) + amount
-//
-//        database.setPlayerBalance(uuid, newSenderBalance.toInt())
-//        database.setPlayerBalance(targetMember.id, newTargetBalance.toInt())
-//
-//        user.openPrivateChannel().queue { channel ->
-//            channel.sendMessage("Вы перевели $amount монет игроку ${targetMember.asMention}.").queue()
-//        }
-//
-//        targetMember.user.openPrivateChannel().queue { channel ->
-//            channel.sendMessage("Игрок ${user.asMention} перевел вам $amount монет.").queue()
-//        }
+        val discordIDSender = user.id
+        println("DIDSender - $discordIDSender")
+        val discordIDTarget = targetMember.id
+        println("DIDTarget - $discordIDTarget")
 
+        val uuidSender = database.getUUIDforDiscordID(discordIDSender)
+        if (uuidSender == null) {
+            event.reply("У вас нет привязанного UUID.").queue()
+            return
+        }
+        println("UUID S - $uuidSender")
+        val uuidTarget = database.getUUIDforDiscordID(discordIDTarget)
+        if (uuidTarget == null) {
+            event.reply("У целевого игрока нет привязанного UUID.").queue()
+            return
+        }
+        println("UUID T - $uuidTarget")
+
+        val senderBalance = database.getPlayerBalance(uuidSender)
+        println("S BALANCE - $senderBalance")
+        if (senderBalance < amount) {
+            user.openPrivateChannel().queue { channel ->
+                event.reply("У вас недостаточно средств.").queue()
+            }
+            return
+        }
+
+        val newSenderBalance = senderBalance - amount
+        println("N S B - $newSenderBalance")
+        val newTargetBalance = database.getPlayerBalance(uuidTarget) + amount
+        println("N T B - $newTargetBalance")
+
+
+        database.setPlayerBalance(uuidSender, newSenderBalance.toInt())
+        database.setPlayerBalance(uuidTarget, newTargetBalance.toInt())
+        user.openPrivateChannel().queue { channel ->
+            channel.sendMessage("Вы перевели $amount монет игроку ${targetMember.asMention}.").queue()
+        }
+        targetMember.user.openPrivateChannel().queue { channel ->
+            channel.sendMessage("Игрок ${user.asMention} перевел вам $amount монет.").queue()
+        }
         event.reply("Команда выполнена успешно.").queue()
-    }
 
+    }
     companion object {
         const val ALLOWED_CHANNEL_ID = 1265343553614250078
     }
